ced7c0906645e32ee15284a222d03f8c7a00f1bf
sangyoon kim <sangyun0904@gmail.com>
2023-07-18 14:46:46 +0900

    terraform ec2 수정

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,17 +19,17 @@ provider "aws" {
 #     key2 = "another value"
 # }
 
-resource "aws_vpc" "skeduler-vpc" {
+resource "aws_vpc" "Store-settlement-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Skeduler VPC"
+    Name = "Store-settlement VPC"
   }
 }
 
 resource "aws_subnet" "public_subnets" {
   count             = length(var.public_subnet_cidrs)
-  vpc_id            = aws_vpc.skeduler-vpc.id
+  vpc_id            = aws_vpc.Store-settlement-vpc.id
   cidr_block        = element(var.public_subnet_cidrs, count.index)
   availability_zone = element(var.azs, count.index)
 
@@ -40,7 +40,7 @@ resource "aws_subnet" "public_subnets" {
 
 resource "aws_subnet" "private_subnets" {
   count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.skeduler-vpc.id
+  vpc_id            = aws_vpc.Store-settlement-vpc.id
   cidr_block        = element(var.private_subnet_cidrs, count.index)
   availability_zone = element(var.azs, count.index)
 
@@ -49,38 +49,39 @@ resource "aws_subnet" "private_subnets" {
   }
 }
 
-resource "aws_internet_gateway" "skeduler-gw" {
-    vpc_id = aws_vpc.skeduler-vpc.id 
+resource "aws_internet_gateway" "Store-settlement-gw" {
+    vpc_id = aws_vpc.Store-settlement-vpc.id 
 
     tags = {
-        Name = "Skeduler VPC IG"
+        Name = "Store-settlement VPC IG"
     }
 }
 
-resource "aws_route_table" "skeduler-second-rt" {
- vpc_id = aws_vpc.skeduler-vpc.id
+resource "aws_route_table" "Store-settlement-second-rt" {
+ vpc_id = aws_vpc.Store-settlement-vpc.id
  
  route {
    cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.skeduler-gw.id
+   gateway_id = aws_internet_gateway.Store-settlement-gw.id
  }
  
  tags = {
-   Name = "Skeduler 2nd Route Table"
+   Name = "Store-settlement 2nd Route Table"
  }
 }
 
 resource "aws_route_table_association" "public_subnet_asso" {
  count          = length(var.public_subnet_cidrs)
  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
- route_table_id = aws_route_table.skeduler-second-rt.id
+ route_table_id = aws_route_table.Store-settlement-second-rt.id
 }
 
-resource "aws_instance" "skeduler-app-server" {
+resource "aws_instance" "Store-settlement-app-server" {
   ami           = "ami-0a306845f8cfbd41a"
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_subnets[0].id
 
   tags = {
-    Name = "Skeduler AppServer Instance"
+    Name = "Store-settlement AppServer Instance"
   }
 }
