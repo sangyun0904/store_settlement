a08c3ed065823379ada68807bc997cb7627344e6
sangyoon kim <sangyun0904@gmail.com>
2023-07-21 09:56:08 +0900

    앱 실행 메모리 확인 로그 추가

--- src/main/java/com/example/storesettlement/StoreSettlementApplication.java
+++ src/main/java/com/example/storesettlement/StoreSettlementApplication.java
@@ -1,21 +1,38 @@
 package com.example.storesettlement;
 
-import com.example.storesettlement.dto.RegisterRequest;
-import com.example.storesettlement.repositories.MemberRepository;
-import com.example.storesettlement.services.AuthenticationService;
-import com.example.storesettlement.services.MemberService;
+import org.apache.commons.logging.Log;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static com.example.storesettlement.model.enums.Role.*;
+import java.text.NumberFormat;
+
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class StoreSettlementApplication {
 
+	private static Log logger = LogFactory.getLog(StoreSettlementApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(StoreSettlementApplication.class, args);
+
+		Runtime runtime = Runtime.getRuntime();
+		final NumberFormat format = NumberFormat.getInstance();
+
+		final long maxMemory = runtime.maxMemory();
+		final long allocatedMemory = runtime.totalMemory();
+		final long freeMemory = runtime.freeMemory();
+		final long mb = 1024 * 1024;
+		final String mega = "MB";
+
+		logger.info("==========================Memory Info============================");
+		logger.info("Free memory: " + format.format(freeMemory / mb) + mega);
+		logger.info("Allocated memory: " + format.format(allocatedMemory / mb) + mega);
+		logger.info("Max memory: " + format.format(maxMemory / mb) + mega);
+		logger.info("Total Free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / mb) + mega);
+		logger.info("=================================================================");
 	}
 
 }
